ef0ec4be7c8b99a68bbab14d871ddb69f6988db4
LuckyTaytee <luqmanhakim00@mail.ugm.ac.id>
2022-06-06 21:33:24 +0700

    VR Lobby, Informasi, navigasi VR

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,9 +8,45 @@ public class MainMenu : MonoBehaviour
 {
     public string preferredDevice = "None";
 
+    void Awake()
+    {
+        if (PlayerPrefs.GetInt("ToggleSelected") == 0)
+        {
+            preferredDevice = "None";
+            Debug.Log("ToggleSelected = 0 + device : none");
+        }
+        else if (PlayerPrefs.GetInt("ToggleSelected") == 1)
+        {
+            preferredDevice = "cardboard";
+            Debug.Log("ToggleSelected = 1 + device : cardboard");
+        } 
+        else
+        {
+            PlayerPrefs.SetInt("ToggleSelected", 0);
+            preferredDevice = "None";
+            Debug.Log("ToggleSelected = 0 + device : none");
+        }
+    }
+    
+    public void ToggleVRMode ()
+    {
+        if (PlayerPrefs.GetInt("ToggleSelected") == 0)
+        {
+            PlayerPrefs.SetInt("ToggleSelected", 1);
+            preferredDevice = "cardboard";
+            Debug.Log("ToggleSelected = 1 + device : cardboard");
+        }
+        else
+        {
+            PlayerPrefs.SetInt("ToggleSelected", 0);
+            preferredDevice = "None";
+            Debug.Log("ToggleSelected = 0 + device : none");
+        }
+    }
+
     public void VRMode ()
     {
-            StartCoroutine(LoadDevice(preferredDevice));
+        StartCoroutine(LoadDevice(preferredDevice));
     }
 
     IEnumerator LoadDevice(string newDevice)
@@ -34,30 +70,16 @@ public class MainMenu : MonoBehaviour
         if (newDevice == "None")
         {
             XRSettings.enabled = false;
+            Debug.Log("Device = None");
         }
         else
         {
             XRSettings.enabled = true;
+            Debug.Log("Device = cardboard");
         }
 
-        //if (newDevice != "None")
-        //{
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        //}
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
-    public void ToggleVRMode ()
-    {
-        if(preferredDevice == "None")
-        {
-            preferredDevice = "cardboard";
-        }
-        else
-        {
-            preferredDevice = "None";
-        }
-    }
-
 
 
     public void QuitGame ()
